311a6cc3b4800f65fd5fcdbe817086d20186330a
RyanWakey <wake52@outlook.com>
2024-03-24 22:38:38 +0000

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
