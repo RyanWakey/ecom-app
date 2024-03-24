2df652e97e82ca8d5484a95270b1e8c858c0d18d
RyanWakey <wake52@outlook.com>
2024-03-24 19:01:23 +0000

    added more routes for product

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Fetch all products
 Route::get('/products', [ProductController::class, 'index']);
+
+// Fetch a single product
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+// Create a new product (protected route)
+Route::post('/products', [ProductController::class, 'store'])->middleware('auth:sanctum');
+
+// Update a product (protected route)
+Route::put('/products/{product}', [ProductController::class, 'update'])->middleware('auth:sanctum');
+
+// Delete a product (protected route)
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->middleware('auth:sanctum');
