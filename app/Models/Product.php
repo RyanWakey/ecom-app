ed0952ad7e61d88492bbba288a0fe36d45fdc70a
RyanWakey <wake52@outlook.com>
2024-04-28 06:10:12 +0100

    Implemented relationship between product image and product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,4 +19,9 @@ class Product extends Model
     {
         return $this->hasMany(CartItem::class);
     }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
--- database/migrations/2024_04_28_050720_create_product_images_table.php
+++ database/migrations/2024_04_28_050720_create_product_images_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('url');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
