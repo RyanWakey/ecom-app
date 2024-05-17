34fe229c99f13696165b5bc41d44047b0d5104c6
RyanWakey <wake52@outlook.com>
2024-05-17 06:25:58 +0100

    Added Register Auth API request

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,28 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     /**
+     * Handle an API register request
+     */
+    public function apiRegister(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:6'],
+        ]);
+
+        $user = User::create($data);
+
+        $request->user()->createToken('API Token')->plainTextToken;
+
+        return [ 
+            'user' => $user,
+            'token' => $token
+        ];
+
+    }
+
+    /**
      * Handle an API authentication request.
      */
     public function apiLogin(Request $request)
