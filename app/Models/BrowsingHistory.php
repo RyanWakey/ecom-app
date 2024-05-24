86bf39b73e0f914430f6154cbde0a1c8c1454cf7
RyanWakey <wake52@outlook.com>
2024-05-24 03:25:13 +0100

    Created Product and User relationship between browsing history

--- app/Models/BrowsingHistory.php
+++ app/Models/BrowsingHistory.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class BrowsingHistory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'product_id', 'viewed_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
