3965fb9adf8243e33a8325df3c1a4218cbcdcd9e
RyanWakey <wake52@outlook.com>
2024-05-24 03:40:05 +0100

    updated model for Deals

--- database/migrations/2024_05_24_023634_create_deals_table.php
+++ database/migrations/2024_05_24_023634_create_deals_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('deals', function (Blueprint $table) {
             $table->id();
+            $table->float('discount');
+            $table->timestamp('valid_until');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
