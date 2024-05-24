ed96efc2385fca02b9e03cd47225ed12af2ecb8c
RyanWakey <wake52@outlook.com>
2024-05-24 03:30:43 +0100

    Added relationship for user and browsingHistory

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(CartItem::class);
     }
+
+    public function browsingHistories()
+    {
+        return $this->hasMany(BrowsingHistories::class);
+    }
 }
