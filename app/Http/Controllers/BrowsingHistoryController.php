761f162a5374a64711314ed113203c870b05b47e
RyanWakey <wake52@outlook.com>
2024-05-24 03:47:30 +0100

    Browsing History Controller index method implemented

--- app/Http/Controllers/BrowsingHistoryController.php
+++ app/Http/Controllers/BrowsingHistoryController.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class BrowsingHistoryController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $userId = $request->user()->id;
+        $history = BrowsingHistory::where('user_id', $userId)->get();
+        return response()->json($history);
+    }
 }
