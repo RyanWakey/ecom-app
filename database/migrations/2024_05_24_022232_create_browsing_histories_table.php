25cbffce3b45db863dbb3baa043b6bbfb48d12d5
RyanWakey <wake52@outlook.com>
2024-05-24 03:24:33 +0100

    Updated Browsing History Model

--- database/migrations/2024_05_24_022232_create_browsing_histories_table.php
+++ database/migrations/2024_05_24_022232_create_browsing_histories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('browsing_histories', function (Blueprint $table) {
             $table->id();
+            $table->timestamp('viewed_at');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
