61cf0035fc020a7b1a587fa8c6ac3e034ec7614a
RyanWakey <wake52@outlook.com>
2024-05-29 06:51:04 +0100

    Updated register api method (no name needed)

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -17,13 +17,11 @@ class AuthController extends Controller
     public function apiRegister(Request $request)
     {
         $data = $request->validate([
-            'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
         $user = User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
