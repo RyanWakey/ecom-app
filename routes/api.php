662d21c3389ffc194ed5f2850002c9090022d4db
RyanWakey <wake52@outlook.com>
2024-06-16 05:47:10 +0100

    Added Route to get garden-essentials

--- routes/api.php
+++ routes/api.php
@@ -92,3 +92,5 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::get('/popular-categories', [CategoryController::class, 'index']);
 // Route to get today's deals
 Route::get('/today-deals', [DealController::class, 'todayDeals']);
+// Route to get Garden-essentials
+Route::get('/garden-essentials', [ProductController::class, 'getGardenEssentials']);
