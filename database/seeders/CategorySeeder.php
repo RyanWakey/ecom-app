e326612a1123b6da7396a0b48a53fbc9010cb59c
RyanWakey <wake52@outlook.com>
2024-07-03 07:08:29 +0100

    Added an image path for the books category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -31,12 +31,13 @@ class CategorySeeder extends Seeder
         $category3 = new Category;
         $category3->name = "Books";
         $category3->description = "Range of books from various genres";
-        $category3->image_path = "images/IECCPWomensGymTopsLongSleeve.jpg";
+        $category3->image_path = "images/MunichWolfBook.jpg";
         $category3->save(); 
 
         $category4 = new Category;
         $category4->name = "Kitchenware";
         $category4->description = "Kitchen appliances and utensils";
+        $category3->image_path = "images/MunichWolfBook.jpg";
         $category4->save(); 
 
          // Create Garden-Essentials as the main category
